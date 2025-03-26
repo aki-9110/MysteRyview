d83be3543cf55ab64a9e905e9c7b5b9cee270e4c
aki-9110 <reokun119@gmail.com>
2025-03-26 16:42:02 +0900

    reviewコントローラのcreateアクションを編集

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,4 +6,28 @@ class ReviewsController < ApplicationController
   def new
     @review = Review.new
   end
+
+  def create
+    book = Book.find_or_create_by(title: review_params[:title], author: review_params[:author])
+    @review = current_user.reviews.build(
+      book_id: book.id,
+      non_spoiler_text: review_params[:non_spoiler_text],
+      spoiler_text: review_params[:spoiler_text],
+      foreshadowing: review_params[:foreshadowing],
+      rating: review_params[:rating]
+    )
+    if @review.save
+      flash[:notice] = "レビューを投稿しました"
+      redirect_to reviews_path
+    else
+      flash[:alert] = "入力に誤りがあります"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :author, :non_spoiler_text, :spoiler_text, :foreshadowing, :rating)
+  end
 end
