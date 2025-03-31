0189ace8eb5689d0610a7283e03517995143d55d
aki-9110 <reokun119@gmail.com>
2025-03-31 16:04:30 +0900

    レビュー編集機能のシステムスペック追記

--- spec/system/reviews_spec.rb
+++ spec/system/reviews_spec.rb
@@ -38,6 +38,23 @@ RSpec.describe "Reviews", type: :system do
         expect(current_path).to eq review_path(review)
       end
     end
+
+    context "レビュー編集画面へアクセス" do
+      it "ログイン画面へ遷移する" do
+        visit edit_review_path(review)
+        expect(page).to have_content "ログインもしくはアカウント登録してください。"
+        expect(current_path).to eq new_user_session_path
+      end
+
+      it "ログインするとレビュー編集画面へ遷移する" do
+        visit edit_review_path(review)
+        fill_in "メールアドレス", with: user.email
+        fill_in "パスワード", with: "password"
+        click_button "ログイン"
+        expect(page).to have_content "ログインしました。"
+        expect(current_path).to eq edit_review_path(review)
+      end
+    end
   end
 
   describe "ログイン後" do
@@ -78,6 +95,45 @@ RSpec.describe "Reviews", type: :system do
         click_button "投稿する"
         expect(page).to have_content "入力に誤りがあります"
         expect(current_path).to eq new_review_path
+      end
+    end
+
+    context "レビューを編集する" do
+      it "レビュー編集ページに遷移できる" do
+        click_link "投稿一覧"
+        click_link href: edit_review_path(review)
+        expect(page).to have_content "レビュー編集"
+        expect(current_path).to eq edit_review_path(review)
+      end
+
+      it "レビューを編集できる" do
+        click_link "投稿一覧"
+        click_link href: edit_review_path(review)
+        fill_in "review[book_title]", with: "そして誰もいなくなった"
+        fill_in "review[book_author]", with: "アガサクリスティー"
+        fill_in "review[non_spoiler_text]", with: "すごかった！"
+        fill_in "review[spoiler_text]", with: "犯人は〇〇です"
+        fill_in "review[foreshadowing]", with: "右腕の傷"
+        fill_in "review[rating]", with: 5
+        click_button "投稿する"
+        expect(page).to have_content "レビューを更新しました"
+        expect(current_path).to eq review_path(review)
+        expect(page).to have_content "書籍: そして誰もいなくなった"
+        expect(page).to have_content "著者: アガサクリスティー"
+      end
+
+      it "レビューの編集に失敗する" do
+        click_link "投稿一覧"
+        click_link href: edit_review_path(review)
+        fill_in "review[book_title]", with: ""
+        fill_in "review[book_author]", with: ""
+        fill_in "review[non_spoiler_text]", with: ""
+        fill_in "review[spoiler_text]", with: "犯人は〇〇です"
+        fill_in "review[foreshadowing]", with: "右腕の傷"
+        fill_in "review[rating]", with: 5
+        click_button "投稿する"
+        expect(page).to have_content "レビューの更新に失敗しました"
+        expect(current_path).to eq edit_review_path(review)
       end
     end
   end
