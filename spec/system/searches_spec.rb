dd7c92934c656352f1be2fcf2d896fc767d80524
aki-9110 <reokun119@gmail.com>
2025-04-02 19:16:00 +0900

    検索機能のシステムスペック記述

--- spec/system/searches_spec.rb
+++ spec/system/searches_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Searches", type: :system do
-  let!(:user) { create(:user) }
-  let!(:matching_review) { create(:review, book_title: "そして誰もいなくなった", book_author: "アガサ・クリスティー", user: user) }
-  let!(:non_matching_review) { create(:review, book_title: "ハリー・ポッター", book_author: "J.K.ローリング", user: user) }
+  let(:user) { create(:user) }
+  let(:book1) {Book.create(title: "そして誰もいなくなった", author: "アガサ・クリスティー")}
+  let(:book2) {Book.create(title: "ハリー・ポッター", author: "J.K.ローリング")}
+  let!(:matching_review) { create(:review, book: book1, user: user) }
+  let!(:non_matching_review) { create(:review, book: book2, user: user) }
 
   before do
     login(user)
@@ -12,8 +14,12 @@ RSpec.describe "Searches", type: :system do
 
   context "検索フォームで検索" do
     it "検索結果に該当するレビューが表示される" do
-      fill_in "", with: "そして誰もいなくなった"
+      fill_in "q[book_title_or_book_author_cont]", with: "そして誰もいなくなった"
       click_button "検索" 
+      expect(page).to have_content("そして誰もいなくなった")
+      expect(page).to have_content("アガサ・クリスティー")
+      expect(page).not_to have_content("ハリー・ポッター")
+      expect(page).not_to have_content("J.K.ローリング")
     end
   end
 end
