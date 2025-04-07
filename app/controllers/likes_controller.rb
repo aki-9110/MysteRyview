55f7657c9110cc919b2426fa2b6538c3ce36e77e
aki-9110 <reokun119@gmail.com>
2025-04-07 20:39:05 +0900

    いいね機能のrspec追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,13 +4,13 @@ class LikesController < ApplicationController
   def create
     review = Review.find(params[:review_id])
     current_user.like(review)
-    redirect_to reviews_path, success: t('.success')
+    redirect_to request.referer, success: t(".success")
   end
 
   def destroy
     like = current_user.likes.find(params[:id])
     review = like.review
     like.destroy!
-    redirect_to reviews_path, success: t('.success'), status: :see_other
+    redirect_to request.referer, success: t(".success"), status: :see_other
   end
 end
