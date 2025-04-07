94cc2da37114d817d1f5a4f8491693f2c73f1f1e
aki-9110 <reokun119@gmail.com>
2025-04-07 16:22:07 +0900

    マイグレーション実行

--- db/migrate/20250407071456_create_likes.rb
+++ db/migrate/20250407071456_create_likes.rb
@@ -1,7 +1,11 @@
 class CreateLikes < ActiveRecord::Migration[7.2]
   def change
     create_table :likes do |t|
+      t.references :user, foreign_key: true
+      t.references :review, foreign_key: true
+
       t.timestamps
     end
+    add_index :likes, [:user_id, :review_id], unique: true
   end
 end
