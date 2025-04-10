e9d030f2ba2cd9af1be7f4e22d919145a5f5f5fd
aki-9110 <reokun119@gmail.com>
2025-04-10 15:58:51 +0900

    自分の投稿ページのrspec追加

--- spec/system/profiles_spec.rb
+++ spec/system/profiles_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Profiles", type: :system do
   let(:user) { create(:user) }
+  let(:user2) { create(:user) }
+  let(:book) { create(:book) }
+  let(:book2) { create(:book) }
+  let!(:review_by_user) { create(:review, user: user, book: book) }
+  let!(:review_by_user2) { create(:review, user: user2, book: book2) }
+
   before do
     login(user)
     click_link "マイページ"
@@ -29,6 +35,16 @@ RSpec.describe "Profiles", type: :system do
       click_button "更新する"
       expect(current_path).to eq(edit_profile_path)
       expect(page).to have_content("ユーザーの更新に失敗しました")
+    end
+  end
+
+  describe "自分の投稿一覧" do
+    it "自分の投稿だけが表示される" do
+      click_link "自分の投稿"
+      sleep 0.5
+      expect(current_path).to eq(my_reviews_profile_path)
+      expect(page).to have_content(review_by_user.book.title)
+      expect(page).not_to have_content(review_by_user2.book.title)
     end
   end
 end
