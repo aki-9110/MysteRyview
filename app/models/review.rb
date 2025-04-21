babda4cfab09312ce2119120bac82a69276a4068
aki-9110 <reokun119@gmail.com>
2025-04-21 17:05:38 +0900

    タグに関するモデルを編集

--- app/models/review.rb
+++ app/models/review.rb
@@ -11,8 +11,35 @@ class Review < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :review_tags, dependent: :destroy
+  has_many :tag, through: :review_tags
 
   has_one_attached :image
+
+  # フォームからのタグの入力を仮想属性として受け取る
+  attr_accessor :tag_names
+
+  before_validation :set_tag  # 保存前に仮想属性をセット
+  after_save :save_tags # 保存後にタグを処理する
+
+  # エラー時に入力内容を保持するために保存前に既存のタグをtag_namesにセットする
+  def set_tag
+    self.tag_names ||= tags.map(&:name).join(', ')
+  end
+
+  def save_tags
+    # tag_namesが未入力の状態なら処理を返す
+    return if tag_names.blank?
+  
+    # カンマ区切りの文字列を配列に変換する
+    tag_list = tag_names.split(',').map(&:strip).uniq
+  
+    # 既存のタグを取得し、なければ作成
+    tag_list.each do |tag_name|
+      tag = Tag.find_or_create_by(name: tag_name)
+      self.tags << tag unless self.tags.include?(tag)
+    end
+  end
 
   # bookのパラメータを含めてreviewを作成するメソッド
   def self.build_with_book(params)
@@ -33,4 +60,6 @@ class Review < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     [ "id", "created_at", "updated_at" ]
   end
+
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,6 @@
 class Tag < ApplicationRecord
+  has_many :review_tags, dependent: :destroy
+  has_many :review, through: :review_tags
+
+  validates :name, presence: true, uniqueness: true
 end
