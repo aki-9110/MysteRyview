7ab5d0bde32c11b6579e78d155934e6719bd4af0
aki-9110 <reokun119@gmail.com>
2025-04-23 17:03:02 +0900

    Lint修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,6 +5,6 @@ class Tag < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "id", "name", "updated_at"]
+    [ "created_at", "id", "name", "updated_at" ]
   end
 end
