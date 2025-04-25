0dfa072ab5e760218f2cd27d9323b28025dec387
aki-9110 <reokun119@gmail.com>
2025-04-25 15:37:00 +0900

    オートコンプリート機能のrspec追記

--- spec/system/searches_spec.rb
+++ spec/system/searches_spec.rb
@@ -10,11 +10,19 @@ RSpec.describe "Searches", type: :system do
   before do
     login(user)
     visit reviews_path
+    sleep 0.5
   end
 
   context "検索フォームで検索" do
+    it "オートコンプリート候補が表示される" do
+      fill_in "q[book_title_or_book_author_or_tags_name_cont]", with: "そして"
+      expect(page).to have_selector("ul[data-autocomplete-target='results'] li", text: "そして誰もいなくなった", wait: 5)
+      expect(page).to have_selector("ul[data-autocomplete-target='results'] li", text: "アガサ・クリスティー", wait: 5)
+      expect(page).not_to have_selector("ul[data-autocomplete-target='results'] li", text: "ハリー・ポッター")
+    end
+
     it "検索結果に該当するレビューが表示される" do
-      fill_in "q[book_title_or_book_author_cont]", with: "そして誰もいなくなった"
+      fill_in "q[book_title_or_book_author_or_tags_name_cont]", with: "そして誰もいなくなった"
       click_button "検索"
       expect(page).to have_content("そして誰もいなくなった")
       expect(page).to have_content("アガサ・クリスティー")
