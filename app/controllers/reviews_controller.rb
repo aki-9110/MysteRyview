8e2cf4acfaca782c65dba08ce9b50e0823537857
aki-9110 <reokun119@gmail.com>
2025-04-25 15:58:35 +0900

    オートコンプリートの不具合を修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :authenticate_user!, except: [ :index, :show, :autocomplete ]
   before_action :set_review, only: [ :edit, :update ]
 
   def index
