461152cbbc3a8631979eae86c0daa72fffb9c0e3
aki-9110 <reokun119@gmail.com>
2025-04-30 15:47:36 +0900

    いいねの通知機能を実装

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,13 @@ class Like < ApplicationRecord
   belongs_to :review
 
   validates :user_id, uniqueness: { scope: :review_id }
+
+  # いいねがデータベースへ保存された後に通知を作成メソッドを実行
+  after_create_commit :create_notification_like
+
+  private
+
+  def create_notification_like
+    Notification.create(visitor_id: self.user_id, visited_id: self.review.user_id, notifiable: self, action: "like")
+  end
 end
